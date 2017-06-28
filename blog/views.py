4357999f5ddfd7b885d1d8007f9e5f91ba251e0a
BertoDiaz <heriberto.diazluis@gmail.com>
2017-06-28 12:58:28 +0200

    Views modified.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Element
 from .forms import ElementForm
